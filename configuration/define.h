93e7bc47c8e6028ffa55d69839207f5cf1e1df6a
icube78 <114530608+icube78@users.noreply.github.com>
2022-09-27 23:17:29 +0200

    Update defs.h

---
+++ configuration/define.h
@@ -0,0 +1,157 @@
+
+#define E
+ImFont* m_pFont;
+
+DWORD_PTR Uworld;
+DWORD_PTR LocalPawn;
+DWORD_PTR PlayerState;
+DWORD_PTR Localplayer;
+DWORD_PTR Rootcomp;
+DWORD_PTR relativelocation;
+DWORD_PTR PlayerController;
+DWORD_PTR Persistentlevel;
+DWORD_PTR Ulevel;
+
+Vector3 localactorpos;
+Vector3 Localcam;
+
+uint64_t TargetPawn;
+int localplayerID;
+
+bool isaimbotting;
+bool CrosshairSnapLines = false;
+bool team_CrosshairSnapLines;
+
+
+RECT GameRect = { NULL };
+D3DPRESENT_PARAMETERS d3dpp;
+
+DWORD ScreenCenterX;
+DWORD ScreenCenterY;
+DWORD ScreenCenterZ;
+
+//static void xCreateWindow();
+//static void xInitD3d();
+//static void xMainLoop();
+//static LRESULT CALLBACK WinProc(HWND hWnd, UINT Message, WPARAM wParam, LPARAM lParam);
+//extern LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+
+static HWND Window = NULL;
+//IDirect3D9Ex* p_Object = NULL;
+static LPDIRECT3DDEVICE9 D3dDevice = NULL;
+static LPDIRECT3DVERTEXBUFFER9 TriBuf = NULL;
+
+
+
+
+DWORD Menuthread(LPVOID in) {
+	while (1) {
+		if (GetAsyncKeyState(VK_INSERT) & 1) {
+			item.show_menu = !item.show_menu;
+		}
+		Sleep(2);
+	}
+}
+
+
+static HWND get_process_wnd(uint32_t pid) {
+	std::pair<HWND, uint32_t> params = { 0, pid };
+
+	BOOL bResult = EnumWindows([](HWND hwnd, LPARAM lParam) -> BOOL {
+		auto pParams = (std::pair<HWND, uint32_t>*)(lParam);
+		uint32_t processId = 0;
+
+		if (GetWindowThreadProcessId(hwnd, reinterpret_cast<LPDWORD>(&processId)) && processId == pParams->second) {
+			SetLastError((uint32_t)-1);
+			pParams->first = hwnd;
+			return FALSE;
+		}
+
+		return TRUE;
+
+		}, (LPARAM)&params);
+
+	if (!bResult && GetLastError() == -1 && params.first)
+		return params.first;
+
+	return NULL;
+}
+ImFont* title;
+ImFont* title1;
+ImFont* otherfont;
+ImFont* tabfont;
+ImFont* othertitle;
+ImFont* spritefont;
+
+void xMainLoop()
+{
+	static RECT old_rc;
+	ZeroMemory(&Message, sizeof(MSG));
+
+	while (Message.message != WM_QUIT)
+	{
+		if (PeekMessage(&Message, Window, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&Message);
+			DispatchMessage(&Message);
+		}
+
+		HWND hwnd_active = GetForegroundWindow();
+
+		if (hwnd_active == hwnd) {
+			HWND hwndtest = GetWindow(hwnd_active, GW_HWNDPREV);
+			SetWindowPos(Window, hwndtest, 0, 0, 0, 0, SWP_NOMOVE | SWP_NOSIZE);
+		}
+
+		if (GetAsyncKeyState(0x23) & 1)
+			exit(8);
+
+		RECT rc;
+		POINT xy;
+
+		ZeroMemory(&rc, sizeof(RECT));
+		ZeroMemory(&xy, sizeof(POINT));
+		GetClientRect(hwnd, &rc);
+		ClientToScreen(hwnd, &xy);
+		rc.left = xy.x;
+		rc.top = xy.y;
+
+		ImGuiIO& io = ImGui::GetIO();
+		io.ImeWindowHandle = hwnd;
+		io.DeltaTime = 1.0f / 60.0f;
+
+		POINT p;
+		GetCursorPos(&p);
+		io.MousePos.x = p.x - xy.x;
+		io.MousePos.y = p.y - xy.y;
+
+		if (GetAsyncKeyState(VK_LBUTTON)) {
+			io.MouseDown[0] = true;
+			io.MouseClicked[0] = true;
+			io.MouseClickedPos[0].x = io.MousePos.x;
+			io.MouseClickedPos[0].x = io.MousePos.y;
+		}
+		else
+			io.MouseDown[0] = false;
+
+		if (rc.left != old_rc.left || rc.right != old_rc.right || rc.top != old_rc.top || rc.bottom != old_rc.bottom)
+		{
+			old_rc = rc;
+
+			Width = rc.right;
+			Height = rc.bottom;
+
+			d3dpp.BackBufferWidth = Width;
+			d3dpp.BackBufferHeight = Height;
+			SetWindowPos(Window, (HWND)0, xy.x, xy.y, Width, Height, SWP_NOREDRAW);
+			D3dDevice->Reset(&d3dpp);
+		}
+		render();
+	}
+	ImGui_ImplDX9_Shutdown();
+	ImGui_ImplWin32_Shutdown();
+	ImGui::DestroyContext();
+
+	DestroyWindow(Window);
+}
+
